7ce4f117b0b527c5657138959b5c9457a310175b
Nallirajah Niruban <nirubannallirajah@gmail.com>
2026-06-03 11:55:10 +0530

    Delete lib/.firebase-config.ts

--- lib/.firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCTodVr3s7LPpuTBnOQdD5aw10HfaWRSC8",
-  authDomain: "foxlaproductions.firebaseapp.com",
-  projectId: "foxlaproductions",
-  storageBucket: "foxlaproductions.firebasestorage.app",
-  messagingSenderId: "619375811190",
-  appId: "1:619375811190:web:44725ea945409512495d57"
-};
